534d13575e299a2e7b0a187cf66ee9c05b49c8c3
Narr <nardgu@gmail.com>
2016-08-09 22:11:10 +0800

    after fe9c709979d5875ddeacafb43af61794cd72f4cc

--- src/platform/browser/providers.ts
+++
@@ -1,9 +0,0 @@
-import { provide } from '@angular/core';
-
-export const APPLICATION_PROVIDERS = [
-  // provide(window, { useValue: window })
-];
-
-export const PROVIDERS = [
-  ...APPLICATION_PROVIDERS
-];
